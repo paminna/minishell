45b9c4ca670b342681c2b6f5532c36836829e91e
Paladin Minna <paminna@si-b3.kzn.21-school.ru>
2021-05-13 13:54:30 +0300

    export is working

--- ft_echo.c
+++ ft_echo.c
@@ -38,21 +38,6 @@ void	ft_pwd()
 	free(pwd);
 }
 
-// int		ft_count_lines(char **env, t_env *env_struct)
-// {
-// 	int i;
-
-// 	i = 0;
-// 	env_struct->max_len = -1;
-// 	while(env[i])
-// 	{
-// 		if (ft_strlen(env[i]) > env_struct->max_len)
-// 			env_struct->max_len = ft_strlen(env[i]);
-// 		i++;
-// 	}
-// 	return (i);
-// }
-
 void	ft_copy_env(char **env, t_env *env_struct)
 {
 	int i;
@@ -86,11 +71,11 @@ void	ft_sort_exp(char **env, t_env *env_struct)
 	{
 		while (j < env_struct->count_lines - i - 1)
 		{
-			if (ft_strcmp(env[j], env[j + 1]) > 0)
+			if (ft_strcmp(env_struct->exp[j], env_struct->exp[j + 1]) > 0)
 			{
-				swap = env[j];
-				env[j] = env[j + 1];
-				env[j + 1] = swap;
+				swap = env_struct->exp[j];
+				env_struct->exp[j] = env_struct->exp[j + 1];
+				env_struct->exp[j + 1] = swap;
 			}
 			j++;
 		}
@@ -107,28 +92,28 @@ void	ft_check_all_keys(t_env *env_struct)
 
 	i = 0;
 	j = 0;
+	while (ft_strcmp(env_struct->env[i], "TMPDIR=/var/folders/zz/zyxvpxvq6csfxvn_n003vdrm00yvf5/T/") > 0)
+		i++;
 	while (env_struct->env[i])
 	{
-		while (env_struct->env[i][j] != '=')
+		while (env_struct->env[i][j])
 		{
-			j++;
-			len_key = --j;
-			if (ft_strncmp(env_struct->env[i], env_struct->key, len_key) == 0)
-				env_struct->flags.new_key = 1;
+			if (env_struct->env[i][j] != '=')
+				j++;
+			else if (env_struct->env[i][j] == '=')
+			{
+				len_key = 0;
+				while (env_struct->env[i][len_key] == env_struct->key[len_key] && len_key < j)
+					len_key++;
+				if (len_key != j - 1)
+					env_struct->flags.new_key = 1;
+				j++;
+			}
 		}
 		j = 0;
 		i++;
 	}
 }
-
-// void	ft_check_value(t_env *env_struct)
-// {
-// 	int i;
-// 	while (env_struct->value[i] != '\0')
-// 		i++;
-// 	if (env_struct->value[i] == '=')
-// 		env_struct->flags.empty_val = 1;
-// }
 
 void	ft_check_exports(t_env *env_struct, char **env)
 {
@@ -143,14 +128,15 @@ void	ft_check_exports(t_env *env_struct, char **env)
 		env_struct->count_lines++;
 	}
 	env_struct->env = (char**)malloc(sizeof(char*) * (env_struct->count_lines + 1));
+	env_struct->exp = (char**)malloc(sizeof(char*) * (env_struct->count_lines + 1));
 	if (!env_struct->env)
 		ft_errors("malloc error");
 	while (i < env_struct->count_lines - 1)
 	{
 		env_struct->env[i] = ft_strdup(env[i]);
+		env_struct->exp[i] = ft_strdup(env[i]);
 		i++;
 	}
-	// ft_check_value(env_struct);
 	if (env_struct->key && env_struct->flags.new_key && env_struct->value)
 	{
 		env_struct->exp[i] = ft_strjoin(env_struct->key, env_struct->value);
@@ -175,13 +161,10 @@ void	ft_copy_exp(char **env, t_env *env_struct)
 	env_struct->count_lines = i;
 	i = 0;
 	ft_check_exports(env_struct, env);
-	// env_struct->exp = (char**)malloc(sizeof(char*) * (env_struct->count_lines + 1));
-	if (!env_struct->exp)
-		ft_errors("malloc error");
 	ft_sort_exp(env, env_struct);
 	while (i < env_struct->count_lines)
 	{
-		env_struct->exp[i] = ft_strjoin("declare -x ", env[i]);
+		env_struct->exp[i] = ft_strjoin("declare -x ", env_struct->exp[i]);
 		i++;
 	}
 }
@@ -211,16 +194,27 @@ void	ft_out_exp(t_env *env_struct)
 {
 	int i;
 	int j;
+	char s;
 
 	i = 0;
 	j = 0;
+	s = 34;
 	while (env_struct->exp[i])
 	{
+		while (env_struct->exp[i][j] != '=')
+		{
+			write(1, &env_struct->exp[i][j], 1);
+			j++;
+		}
+		write(1, &env_struct->exp[i][j], 1);
+		j++;
+		write(1, &s, 1);
 		while (env_struct->exp[i][j])
 		{
 			write(1, &env_struct->exp[i][j], 1);
 			j++;
 		}
+		write(1, &s, 1);
 		write(1, "\n", 1);
 		j = 0;
 		i++;
@@ -237,30 +231,31 @@ int main(int argc, char **argv, char **env)
 {
 	t_all *all;
 	t_env env_struct;
-	// int i = 0;
+
+	env_struct.key = "name";
+	env_struct.value = "=value";
+	// all->result[3] = (char*)malloc(5);
+	// all->result[3] = "exp";
 	ft_init_flags(&env_struct);
 	ft_copy_env(env, &env_struct);
 	ft_copy_exp(env, &env_struct);
 	(void)argc;
 	(void)argv;
-	all = (t_all*)ft_calloc(1, sizeof(t_all));
-	all->result = (char**)malloc(sizeof(char**) * 100);
+	// all = (t_all*)ft_calloc(1, sizeof(t_all));
+	// all->result = (char**)malloc(sizeof(char**) * 100);
 	// all->result[0] = "echo";
 	// all->result[1] = (char*)malloc(4);
 	// all->result[1] = "pwd";
 	// all->result[2] = (char*)malloc(4);
 	// all->result[2] = "env";
 	// all->result[3] = (char*)malloc(4);
-	env_struct.key = "name";
-	env_struct.value = "=value";
-	all->result[3] = "exp";
-	all->flag = 0;
+	// all->flag = 0;
 	// if (ft_strncmp(all->result[0], "echo", 3) == 0)
 	// 	ft_echo(all->result[0], all->flag);
 	// if (ft_strncmp(all->result[1], "pwd", 3) == 0)
 	// 	ft_pwd();
 	// if (ft_strncmp(all->result[2], "env", 3) == 0)
 	// 	ft_out_env(all->result[2], &env_struct);
-	if (ft_strncmp(all->result[3], "exp", 3) == 0)
+	// if (ft_strncmp(all->result[3], "exp", 3) == 0)
 		ft_out_exp(&env_struct);
 }
