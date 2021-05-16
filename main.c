194030653643d4b99272d081c21b213f87739abb
Юля <75695866+kaleexand@users.noreply.github.com>
2021-05-16 13:20:24 +0300

    +parser_export
    + функцию для ошибок +очищение памяти

--- main.c
+++ main.c
@@ -12,6 +12,13 @@ size_t	ft_strlen(const char *str)
 	return (i);
 }
 
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (fd < 0 || s == NULL)
+		return ;
+	write(fd, s, ft_strlen(s));
+}
+
 int	ft_strncmp(const char *str1, const char *str2, size_t n)
 {
 	size_t i;
@@ -60,38 +67,100 @@ int	ft_isdigit(int ch)
 	return (0);
 }
 
-void	parser_export(char **massiv)
+int	ft_isalpha(int ch)
+{
+	if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+		return (1);
+	return (0);
+}
+
+char	*ft_strdup(const char *str)
+{
+	char	*str1;
+	int		i;
+	int		len;
+
+	len = 0;
+	i = 0;
+	while (str[len] != '\0')
+		len++;
+	str1 = (char*)malloc(sizeof(*str) * (len + 1));
+	if (!str1)
+		return (NULL);
+	while (i < len)
+	{
+		str1[i] = str[i];
+		i++;
+	}
+	str1[i] = '\0';
+	return (str1);
+}
+
+char	*parser_key_export(char *value)
+{
+	int len_key;
+	char *save_v;
+	char *key;
+	int count;
+
+	len_key = 0;
+	save_v = value;
+	count = -1;
+	while(*value != '\0')
+	{
+		if(*value == '=')
+			break;
+		len_key++;
+		value++;
+	}
+	value = save_v;
+	key = (char*)malloc((sizeof(char) * len_key) + 1);
+	if (key == NULL)
+		exit(-1); //TODO придумать функцию по очистке и выводе ошибок
+		//error("Malloc error", 13);
+	while(++count < len_key)
+	{
+		if(*value == '=')
+			break;
+		key[count] = value[count];
+	}
+	return(key);
+}
+
+void	parser_export2(char *massiv)
 {
 	int i;
-	int j;
-	int flag; //если есть равно 1
+	int sm; // 1 если ошибка
 	t_list_env struct_export;
 
-	j = 0;
-	i = 1;
-	flag = 0;
+	struct_export.value = ft_strdup(massiv);
+	struct_export.key = parser_key_export(struct_export.value);
+	i = 0;
+	if (ft_isdigit(struct_export.key[i]) ||
+		(!ft_isalpha(struct_export.key[i]) && struct_export.key[i] != '_')) // если ПЕРВЫЙ символ в ключе цифры или символ, кроме _ : ошибка
+		sm = 1;
+	i = -1;
+	while (struct_export.key[++i]) //если в ключе содержаться символы кроме букв, цифр и _ ошибка
+	{
+		if (!ft_isdigit(struct_export.key[i]) && !ft_isalpha(struct_export.key[i]) && struct_export.key[i] != '_')
+			sm = 1;
+	}
+	if (sm == 1) // в случае ошибки в ключе
+	{
+		write(2, "Minishell>> ", 12);
+		write(2,"export: ", 8);
+		write(2,"\'", 1);
+		ft_putstr_fd(massiv, 2);
+		write(2,"\'", 1);
+		write(2," not a valid identifier", 23);
+		return ;
+	}
+}
 
-	if (ft_isdigit(massiv[i][j]))
-	{
-		write(1, "export: `5=': not a valid identifier", 37); // нужна отдельная функция для ошибки
-		exit(-1);
-	}
-	while(massiv[i][j] != '\0')
-	{
-		if(massiv[i][j] == '=')
-			flag = 1; // отметили что есть =
-		j++;
-	}
-	if(flag == 1)
-	{
-		while(massiv[i][j] != '=')
-		{
-			struct_export.key = malloc(sizeof(char) * (ft_strlen(j) + 1));
-			struct_export.key = massiv[i][j];
-			j++;
-		}
-	}
-	write(1, &struct_export.key, 1);
+void parser_export(char **massiv)
+{
+	if(massiv[1] != NULL)
+		parser_export2(massiv[1]);
 }
 
 void builtin_function_or_no(t_all *all)
