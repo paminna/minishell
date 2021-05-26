501b270eb823b4e1c1325aadcb5518b794f95e7d
Юля <75695866+kaleexand@users.noreply.github.com>
2021-05-26 16:44:53 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,38 +1,5 @@
 #include "mini.h"
 
-size_t	ft_strlen(const char *str)
-{
-	size_t i;
-
-	if (!str)
-		return (0);
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-void	ft_putstr_fd(char *s, int fd)
-{
-	if (fd < 0 || s == NULL)
-		return ;
-	write(fd, s, ft_strlen(s));
-}
-
-int	ft_strncmp(const char *str1, const char *str2, size_t n)
-{
-	size_t i;
-
-	i = 0;
-	while ((str1[i] || str2[i]) && i < n)
-	{
-		if (str1[i] != str2[i])
-			return ((unsigned char)str1[i] - (unsigned char)str2[i]);
-		i++;
-	}
-	return (0);
-}
-
 void	parser_echo(char **massiv)
 {
 	int	flag;
@@ -60,42 +27,6 @@ void	parser_echo(char **massiv)
 		write(1, "\n", 1);
 }
 
-int	ft_isdigit(int ch)
-{
-	if (ch >= '0' && ch <= '9')
-		return (1);
-	return (0);
-}
-
-int	ft_isalpha(int ch)
-{
-	if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
-		return (1);
-	return (0);
-}
-
-char	*ft_strdup(const char *str)
-{
-	char	*str1;
-	int		i;
-	int		len;
-
-	len = 0;
-	i = 0;
-	while (str[len] != '\0')
-		len++;
-	str1 = (char*)malloc(sizeof(*str) * (len + 1));
-	if (!str1)
-		return (NULL);
-	while (i < len)
-	{
-		str1[i] = str[i];
-		i++;
-	}
-	str1[i] = '\0';
-	return (str1);
-}
-
 char	*parser_key_export(char *value)
 {
 	int len_key;
@@ -117,7 +48,7 @@ char	*parser_key_export(char *value)
 	key = (char*)malloc((sizeof(char) * len_key) + 1);
 	if (key == NULL)
 		exit(-1); //TODO придумать функцию по очистке и выводе ошибок
-		//error("Malloc error", 13);
+	//error("Malloc error", 13);
 	while(++count < len_key)
 	{
 		if(*value == '=')
@@ -213,85 +144,13 @@ void parser_export(char **massiv)
 		parser_export2(massiv[1]);
 }
 
-void builtin_function_or_no(t_all *all)
+void find_command(t_all *all)
 {
 	if (!ft_strncmp(all->result[0], "echo", 5))
 		parser_echo(all->result);
-		// else if (!ft_strncmp(all->str[0], "cd", 3))
-		// 	my_cd(all);
-		// else if (!ft_strncmp(all->str[0], "pwd", 4))
-		// 	my_pwd(all);
 	else if (!ft_strncmp(all->result[0], "export", 7))
 		parser_export(all->result);
-	// else if (!ft_strncmp(all->str[0], "unset", 6))
-	// 	my_unset(all);
-	// else if (!ft_strncmp(all->str[0], "env", 4))
-	// 	my_env(all);
-	// else if (!ft_strncmp(all->str[0], "exit", 5))
-	// 	my_exit(all);
-	// else if (!ft_strncmp(all->str[0], "kill", 5))
-	// 	return ;
-	// else
-	// 	work_with_execve(all);
 }
-
-t_list	*ft_lstnew(void *content)
-{
-	t_list	*list;
-
-	if ((list = (t_list *)malloc(sizeof(t_list))))
-	{
-		list->content = content;
-		list->next = NULL;
-	}
-	return (list);
-}
-
-void	ft_lstadd_back(t_list **lst, t_list *new)
-{
-	t_list *temp_lst;
-	if (!new)
-		return ;
-	if (lst && *lst)
-	{
-		temp_lst = *lst;
-		while (temp_lst->next)
-			temp_lst = temp_lst->next;
-		temp_lst->next = new;
-	}
-	else
-		*lst = new;
-}
-
-void	*ft_memset(void *memptr, int val, size_t len)
-{
-	size_t i;
-
-	i = 0;
-	while (i < len)
-	{
-		*(unsigned char *)(memptr + i) = (unsigned char)val;
-		i++;
-	}
-	return (memptr);
-}
-
-void	ft_bzero(void *str, size_t n)
-{
-	ft_memset(str, 0, n);
-}
-
-void	*ft_calloc(size_t count, size_t size)
-{
-	void *pointer;
-
-	pointer = (void *)malloc(count * size);
-	if (!pointer)
-		return (NULL);
-	ft_bzero(pointer, count * size);
-	return (pointer);
-}
-
 
 char *skip_space(char *str)
 {
@@ -300,58 +159,7 @@ char *skip_space(char *str)
 	return(str);
 }
 
-//char *single_quotes(*str)
-//{
-//	int		i;
-//	int		len;
-//	char	*argument;
-//
-//	//написать с изменением строки
-//	i = 0;
-//	len = 0;
-//	while (str[i + len] != '\0' && str[i + len] != '\'')
-//	{
-//		len++;
-//	}
-//	argument = (char *)malloc(sizeof(char) * (len + 1));
-//	while (i < len)
-//	{
-//		argument[i] = str[i];
-//		i++;
-//	}
-//	return(argument);
-//}
 
-char *double_quotes(char **str)
-{
-	int i;
-	char	*tmp;
-
-	i = 0;
-	(*str)++;
-	tmp = *str;
-	while(tmp[i] && tmp[i] != '\"')
-		i++;
-	tmp = ft_strn(str, i);
-	(*str)++;
-	return(tmp); ////peredelat'
-}
-//
-//char *dollar(*str)
-//{
-//
-//	return(str);
-//}
-//
-//char *skip_spec(char *str)
-//{
-//	if (str == '\'')
-//		single_quotes(str++);
-//	else if (str == '"')
-//		double_quotes(str++);
-//	else if(str == '$')
-//		dollar(str++);
-//}
 char *ft_list(char **str)
 {
 	int len;
@@ -398,38 +206,13 @@ static void	ft_free_arr(char **arr)
 	arr = NULL;
 }
 
-//void	ft_quit(int sig)
-//{
-//	int		pid;
-//	int		status;
-//
-//	(void)sig;
-//	pid = waitpid(-1, &status, WNOHANG);
-//	if (!pid)
-//		write(1,"Quit: 3", 7);
-//}
-//
-//void	ft_sigint(int sig)
-//{
-//	int		pid;
-//	int		status;
-//
-//	pid = waitpid(-1, &status, WNOHANG);
-//	if (sig == SIGINT)
-//	{
-//		write(1, "\n", 2);
-//		if (pid)
-//			write (1, "Minishell>> ", 12);
-//	}
-//}
-
 void	check_sim(char *str)
 {
 	int i;
 
 	i = 0;
 	if ((*str == ';' || *str == '|' || *str == '&' || *str == ')'
-		|| *str == '<' || *str == '>') && str[++i] == '\0')
+		 || *str == '<' || *str == '>') && str[++i] == '\0')
 	{
 		write(2, "Minishell>> ", 12);
 		write(2,"syntax error near unexpected token ", 35);
@@ -466,10 +249,68 @@ void	check_sim(char *str)
 	}
 }
 
-char *parser_result(t_all *all, char *str)
+char	check_spec_s(char c)
 {
+	if (c == '\"')
+		return (c);
+	return (0);
+}
 
-	return(str);
+char *new_str_with_q(char **str, int cnt_quot)
+{
+	char *save;
+	char *s;
+	int i;
+	int n;
+	char d_q;
+
+	save = *str;
+	d_q = '\"';
+	s = (char*)malloc(sizeof(char) * (cnt_quot + 1));
+	i = 0;
+	n = 0;
+	while (i < cnt_quot)
+	{
+		if(d_q == '\"' && check_spec_s(save[n + 1]) && save[n] == '\\')
+			s[i++] = check_spec_s(save[++n]);
+		if(save[n] != '\"')
+			s[i++] = save[n];
+		n++;
+	}
+	s[i] = '\0';
+	*str = *str + cnt_quot;
+	return(s);
+
+
+
+}
+char *check_double_quotes(char **str)
+{
+	int i;
+	int cnt_quot;
+	char *s;
+	char *d;
+
+	i = 0;
+	cnt_quot = 0;
+	s = ++(*str);
+	while (s[i] && s[i] != '\"')
+	{
+		if (s[i] != '\"')
+			cnt_quot++;
+		i++;
+	}
+	s = new_str_with_q(str, cnt_quot);
+	*str += cnt_quot - i;
+	if (**str != 0)
+		*str++;
+	return (s);
+}
+
+char *parser_result(char **str)
+{
+	if(**str == '\"')
+		return(check_double_quotes(str));
 }
 
 //typedef struct s_pare
@@ -482,6 +323,21 @@ char *parser_result(t_all *all, char *str)
 //{
 //	return((t_pare){.str1 = "abc", .str2 = "dce"});
 //};
+
+char	*strjoin_free(char *str1, char *str2)
+{
+	char	*p;
+
+	if (!str1)
+		return (str2);
+	if (!str2)
+		return (str1);
+	p = ft_strjoin(str1, str2);
+	free(str1);
+	free(str2);
+	return (p);
+}
+
 t_all *parser(char *str, t_all *all)
 {
 	t_list	*words = NULL;
@@ -490,65 +346,38 @@ t_all *parser(char *str, t_all *all)
 	//int		len;
 	char	*s;
 
-//	signal(SIGQUIT, ft_quit);
-//	signal(SIGINT, ft_sigint);
 	count = 0;
 	while (*str != '\0')
 	{
-//		len = 0;
 		str = skip_space(str);
 		check_sim(str);
-		//str = parser_result(all, str);
-//		while (str[len] != '\0' && str[len] != ' ' && str[len] != ';')
-//			len++;
+		s = parser_result(&str);
+//		while (*str != '\0' && *str != ' ' && *str != ';' && *str != '|')
+//			s = strjoin_free(s,  parser_result(&str));
 
-
-//	if (*str == ';')
-//		{
-//			all->result = malloc(sizeof(char *) * (i + 1));
-//			j = 0;
-//			while (j < i)
-//			{
-//				all->result[j++] = words->content;
-//				words = words->next;
-//			}
-//			builtin_function_or_no(all);
-//			ft_free_arr(all->result);
-//			//
-//			//
-//			i = 0;
-//
-//		}
-		// if (*str == '\"')
-		//	 s = double_quotes(&str);
-		// else if (*str == '\'')
-		//	 //ft_;
-		//	 ;
-		// else
 		s = ft_list(&str);
 		ft_lstadd_back(&words, ft_lstnew(s));
 		count++;
-		//str += len;
 		str = skip_space(str);
-		if (*str == ';')
-		{
-			if (count != 0)
-			{
-				all->result = malloc(sizeof(char *) * (count + 1));
-				all->result[count] = NULL;
-				j = 0;
-				while (j < count)
-				{
-					all->result[j++] = words->content;
-					words = words->next;
-				}
-				if (ft_strlen(all->result[0]) > 0)
-					builtin_function_or_no(all);
-				ft_free_arr(all->result);
-				count = 0;
-				str++;
-			}
-		}
+//		if (*str == ';')
+//		{
+//			if (count != 0)
+//			{
+//				all->result = malloc(sizeof(char *) * (count + 1));
+//				all->result[count] = NULL;
+//				j = 0;
+//				while (j < count)
+//				{
+//					all->result[j++] = words->content;
+//					words = words->next;
+//				}
+//				if (ft_strlen(all->result[0]) > 0)
+//					find_command(all);
+//				ft_free_arr(all->result);
+//				count = 0;
+//				str++;
+//			}
+//		}
 	}
 	if (count != 0)
 	{
@@ -560,34 +389,106 @@ t_all *parser(char *str, t_all *all)
 			words = words->next;
 		}
 		if (ft_strlen(all->result[0]) > 0)
-			builtin_function_or_no(all);
+			find_command(all);
 		ft_free_arr(all->result);
 	}
-	//
-//	 j = 0;
-	//  while (all->result[j])
-	//  	printf("%s\n", all->result[j++]);
-	//
-
-//// почистить листы (ft_lstlclear)
 	return (all);
+}
+
+void	ft_quit(int sig)
+{
+	int		pid;
+	int		status;
+
+	(void)sig;
+	pid = waitpid(-1, &status, WNOHANG);
+	if (!pid)
+		ft_putendl_fd("Quit: 3", 1);
+}
+
+void	ft_sigint(int sig)
+{
+	int pid;
+	int status;
+
+	pid = waitpid(-1, &status, WNOHANG);
+	if (sig == SIGINT)
+	{
+		write(1,"\n", 2);
+		if (pid)
+			write (1, "Minishell>> ", 12);
+	}
+}
+
+struct termios	init(void)
+{
+	struct termios	term;
+
+	tcgetattr(0, &term);
+	term.c_lflag &= ~(ECHO);
+	term.c_lflag &= ~(ICANON);
+	tcsetattr(0, TCSANOW, &term);
+	return (term);
+}
+
+void	error(char *str, int error)
+{
+	char *id;
+	char *res;
+
+	ft_putendl_fd(str, 2);
+	id = ft_itoa(error);
+	res = ft_strjoin("error=", id);
+//	export_var(res);
+	free(id);
+	free(res);
+	exit(error);
+}
+
+int	make_fd(void)
+{
+	int fd;
+
+	fd = open("minishell_history", O_CREAT, S_IWRITE | S_IREAD);
+	if (!(fd))
+		error("Error when creating a file history", 1);
+	fd = open("minishell_history", O_RDWR | O_TRUNC);
+	if (!fd)
+		error("Error occurred while opening the history file", 1);
+	return (fd);
+}
+
+static void ft_init(t_parser *par)
+{
+	par->history = ft_calloc(500, sizeof(char*));
+	if(!par->history)
+		error("Allocated error", 11);
+	par->str = ft_calloc(2, sizeof(char*));
+	if(!par->str)
+		error("Allocated error", 11);
+	par->buf = NULL;
 }
 
 int main(int ac, char **av, char **env)
 {
 	char	str[500];
 	int		i;
-	t_list	*poka_tak;
 	t_all	*all;
+	struct termios	term;
+	int				fd;
+	t_parser pars;
 
-	write (1, "Minishell>> ", 12);
+	dup2(STDOUT_FILENO, 3);
+	dup2(STDIN_FILENO, 4);
+	signal(SIGQUIT, ft_quit);
+	signal(SIGINT, ft_sigint); //ctrl c
+	term = init();
+	fd = make_fd();
+	ft_init(&pars);
+	write(1, "Minishell>> ", 12);
 	all = (t_all *) ft_calloc(1, sizeof(t_all));
 	i = read(0, str, 1000);
 	str[i - 1] = '\0';
-	// poka_tak = (t_list *)ft_calloc(1, sizeof(t_list));
-	// get_env(env, poka_tak);
 	parser(str, all);
-	//write(1, "\n", 1);
-	// ft_builtin_or_no(av);
 	return 0;
 }
